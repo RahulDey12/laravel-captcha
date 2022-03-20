513dce710fd9261da1563a7667bbd712fbe912cb
Rahul Dey <rahul900day@gmail.com>
2022-03-20 13:57:36 +0530

    added captcha config file doc comment.

--- config/captcha.php
+++ config/captcha.php
@@ -46,9 +46,45 @@ return [
     */
     'secret' => env('CAPTCHA_SECRET_KEY', ''),
 
+    /*
+    |------------------------------------------------------------
+    | Captcha Locale Configuration
+    |------------------------------------------------------------
+    |
+    | The captcha locale determines the default locale of your
+    | Captcha. You can add any locale value that supported
+    | by your captcha provider.
+    |
+    | ReCaptcha Language List: https://developers.google.com/recaptcha/docs/language
+    | HCaptcha Language List: https://docs.hcaptcha.com/languages
+    |
+    */
     'locale' => env('CAPTCHA_LOCALE', 'en'),
 
+    /*
+    |------------------------------------------------------------
+    | Captcha Theme
+    |------------------------------------------------------------
+    |
+    | The captcha theme is the default theme that will display
+    | the captcha checkbox.
+    |
+    | Supported: "light", "dark"
+    |
+    */
     'theme' => env('CAPTCHA_THEME', 'light'),
 
+    /*
+    |------------------------------------------------------------
+    | Captcha Size
+    |------------------------------------------------------------
+    |
+    | The captcha size is the default size of the captcha
+    | checkbox. According to your style preference you
+    | can use any of the supported captcha size.
+    |
+    | Supported: "normal", "compact"
+    |
+    */
     'size' => env('CAPTCHA_SIZE', 'normal'),
 ];
